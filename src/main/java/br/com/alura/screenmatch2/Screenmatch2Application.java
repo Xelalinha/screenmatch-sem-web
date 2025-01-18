1149fd8011bcd158c61381b6813b54705cee3a0c
Alex Araujo <alexgabriel.araujo@hotmail.com>
2025-01-18 17:12:52 -0300

    Inicio do Projeto Spring

---
+++ src/main/java/br/com/alura/screenmatch2/Screenmatch2Application.java
@@ -0,0 +1,26 @@
+package br.com.alura.screenmatch2;
+
+import br.com.alura.screenmatch2.Service.ConsumoAPI;
+import br.com.alura.screenmatch2.Service.ConverteDados;
+import br.com.alura.screenmatch2.model.DadosSerie;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Screenmatch2Application  implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Screenmatch2Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoAPI = new ConsumoAPI();
+		var json = consumoAPI.obterDados("https://www.omdbapi.com/?apikey=469368ce&t=The+Witcher");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
